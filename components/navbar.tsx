9eba3b296d7addc7a410b07eaaa2b6b238dc1778
Ruthskery <cyrilimperial12@gmail.com>
2025-03-11 19:45:01 +0800

    Navbar first

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Gabarda Pogi</div>;
+  return <div>Navbar Here</div>;
 };
 
 export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,8 @@ import React from 'react';
 
 const Home = () => {
   return <div>
-    <h1>Home</h1>
-    <p>Welcome to the Home Page</p>
     <Navbar />
+    Home Page
   </div>;
 };
 
