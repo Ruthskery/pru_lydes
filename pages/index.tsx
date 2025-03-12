aa8a46afc9f68a53575361ddb0192883c2a60855
Ruthskery <cyrilimperial12@gmail.com>
2025-03-12 08:50:05 +0800

    Components Added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
+import About from '@/components/about';
+import Contact from '@/components/contact';
+import Footer from '@/components/footer';
+import Hero from '@/components/hero';
 import Navbar from '@/components/navbar';
 import React from 'react';
 
 const Home = () => {
   return <div>
     <Navbar />
-    Home Page
+    <Hero />
+    <About />
+    <Contact />
+    <Footer />
   </div>;
 };
 
