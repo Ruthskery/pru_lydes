fdfc5656f237d5e449a11b6e184727348617c15e
Ruthskery <cyrilimperial12@gmail.com>
2025-03-15 13:03:30 +0800

    credentials and careerpath added

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Footer from '@/components/footer';
 import Hero from '@/components/hero';
 import Navbar from '@/components/navbar';
 import Carousel from '@/components/carousel';
+import Credentials from '@/components/credentials';
+import Careerpath from '@/components/careerpath';
 import React from 'react';
 
 const Home = () => {
@@ -12,6 +14,8 @@ const Home = () => {
     <Hero />
     <Carousel />
     <About />
+    <Credentials />
+    <Careerpath />
     <Contact />
     <Footer />
   </div>;
