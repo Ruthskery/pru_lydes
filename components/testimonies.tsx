013bfe01925ae4fe8e5e10681e68c5017845b1fc
Ruthskery <cyrilimperial12@gmail.com>
2025-03-19 16:53:08 +0800

    Update testimonies.tsx

--- components/testimonies.tsx
+++ components/testimonies.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { UserCircle } from "lucide-react"; // Default Profile Icon
