0528210266bd79561672a7a2519ed9200a054a3a
Japsuii <finaly63@gmail.com>
2025-03-26 15:38:36 +0800

    Appointment draggrable

--- components/appointment.tsx
+++ components/appointment.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useEffect, useState, useRef, forwardRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { PopupWidget } from "react-calendly";
 import Draggable from "react-draggable";
+import { Calendar } from "lucide-react";
 
 const DraggableCalendlyPopup = () => {
   const [isClient, setIsClient] = useState(false);
@@ -15,12 +16,14 @@ const DraggableCalendlyPopup = () => {
 
   return (
     <Draggable nodeRef={nodeRef} handle=".drag-handle" bounds="parent">
-      <div ref={nodeRef} className="fixed bottom-10 right-10 cursor-move active:cursor-move w-70">
-        <div className="drag-handle w-5 h-5 bg-black rounded-full cursor-move ml-65"></div>
+      <div ref={nodeRef} className="fixed bottom-10 right-10 cursor-move active:cursor-move w-70 z-60">
+        <div className="absolute drag-handle flex items-center justify-center w-13 h-13 bg-red-500 rounded-full cursor-move bottom-3 right-5  z-2147483648">
+          <Calendar className="text-white w-7 h-7" />
+        </div>
         <PopupWidget
           url="https://calendly.com/mikotothemax/30min"
           rootElement={document.body}
-          text="Schedule a Meeting"
+          text="Schedule a Meeting‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ."
           textColor="#000000"
           color="#FFD700"
         />
